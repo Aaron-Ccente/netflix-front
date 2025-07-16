30694ea54d1b2cf684b94e1200d53b9da459b283
Aaron-Ccente <aronccente@gmail.com>
2025-07-15 21:05:23 -0500

    Carpeta utils agregada

---
+++ src/utils/convertir64.js
@@ -0,0 +1,60 @@
+export const convertImageToWebP = (file) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+
+      img.onload = () => {
+        // Calcular el tamaño máximo permitido
+        let { width, height } = img;
+        const MAX_WIDTH = 1200;
+        const MAX_HEIGHT = 800;
+        
+        if (width > MAX_WIDTH || height > MAX_HEIGHT) {
+          const ratio = Math.min(MAX_WIDTH / width, MAX_HEIGHT / height);
+          width = Math.round(width * ratio);
+          height = Math.round(height * ratio);
+        }
+        
+        canvas.width = width;
+        canvas.height = height;
+        ctx?.drawImage(img, 0, 0, width, height);
+
+       
+        canvas.toBlob(
+          (blob) => {
+            if (blob) {
+              resolve(blob);
+            } else {
+              reject(new Error("Error al convertir la imagen a WebP"));
+            }
+          },
+          "image/webp",
+          0.8 // Reduce la calidad al 80%
+        );
+      };
+
+      img.onerror = () => {
+        reject(new Error("Error al cargar la imagen"));
+      };
+
+      img.src = URL.createObjectURL(file);
+    });
+  };
+
+  export const convertToBase64 = (blob) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (typeof reader.result === "string") {
+          resolve(reader.result);
+        } else {
+          reject(new Error("Error al convertir a Base64"));
+        }
+      };
+      reader.onerror = () => {
+        reject(new Error("Error al leer el archivo"));
+      };
+      reader.readAsDataURL(blob);
+    });
+  };
