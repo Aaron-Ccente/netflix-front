ccdf3d96f0cf03e1e0ccbf0b585a4485c24ed07c
Aaron-Ccente <aronccente@gmail.com>
2025-07-15 21:08:23 -0500

    Estilos actualizados en la page

--- src/Components/Dashboard/Productores/page.js
+++ src/Components/Dashboard/Productores/page.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import EditIcon from "Icons/EditIcon";
 import DeleteIcon from "Icons/DeleteIcon";
-import AddIcon from "Icons/AddIcon";
 import AddNewCompany from "./AddNewCompany";
 import axios from "axios";
 import FormEditCompany from "./FormEditCompany";
+import CompanyIcon from "Icons/CompanyIcon";
 function Page() {
   const [addNewCompany, setAddNewCompany] = useState(false);
   const [deleteCompany, setDeleteCompany] = useState(false);
@@ -39,44 +39,36 @@ function Page() {
     setDeleteCompany(false);
   }
   return (
-    <div className="h-[calc(100vh-78px)] flex flex-col p-8">
-    <div className="flex justify-between items-center mb-4 flex-shrink-0">
-      <h2 className="text-2xl font-bold">Lista de compañias</h2>
-      <button
-        className="bg-white px-4 h-fit py-1 font-bold rounded-lg flex text-black gap-2"
-        onClick={handleAddNewCompany}
-      >
-        <AddIcon />
-        Añadir nueva compañia
-      </button>
-    </div>
-
-    {addNewCompany && (<AddNewCompany viewModal={handleAddNewCompany} />)}
-
-    <div className="overflow-x-auto pr-4 scroll-red rounded-b-3xl">
-      <table className="min-w-full text-black border-separate border-spacing-y-4">
-          <thead className="shadow-md shadow-red-600 bg-white">
+    <div className="p-8">
+      <div className='flex justify-between items-center'>
+        <h2 className="text-2xl font-bold mb-6 text-[#e2e5e5]">Lista de Compañías</h2>
+        <button className='bg-[#176b81] px-4 h-fit py-3 font-bold rounded-lg flex text-[#e2e5e5] gap-2 hover:scale-105 hover:transition-transform' onClick={handleAddNewCompany} ><CompanyIcon/>Añadir nueva compañía</button>
+      </div>
+      {addNewCompany && <AddNewCompany viewModal={handleAddNewCompany}/>} 
+      <div className="overflow-x-auto mt-4">
+        <table className="min-w-full border-separate">
+          <thead className='text-[#2ec7bc] bg-[#0c161e] rounded-tl-3xl rounded-tr-3xl '>
             <tr>
-              <th className="py-2 px-4 rounded-tl-3xl">ID</th>
-              <th className="py-2 px-4 ">Nombre de compañia</th>
-              <th className="py-2 px-4 rounded-tr-3xl">Acciones</th>
+              <th className="py-2 px-4 rounded-tl-3xl border-t-2 border-l-2 border-b-2 border-[#334155]">ID</th>
+              <th className="py-2 px-4 border-t-2 border-l-2 border-b-2 border-[#334155]">Nombre de Compañía</th>
+              <th className="py-2 px-4 rounded-tr-3xl border-2 border-[#334155]">Acciones</th>
             </tr>
           </thead>
-          <tbody className="bg-white">
+          <tbody className='bg-[#0c161e] text-[#e2e5e5]'>
             {allCompanies.map((element, index) =>
-              <tr className="border-b border-gray-300" key={index}>
-                <td className="py-2 px-4 text-center">{element.id}</td>
-                <td className="py-2 px-4 text-center">{element.name}</td>
-                <td className="py-2 px-4 text-center flex justify-center">
-                  <button onClick={()=>handleEditCompany(element.id, element.name)} className="bg-blue-600 text-white px-3 py-1 w-fit rounded mr-2 flex">
-                    <EditIcon />
-                    Editar
+              <tr key={index}>
+                <td className="py-2 px-4 text-center border-l-2 border-b-2 border-[#334155]">{element.id}</td>
+                <td className="py-2 px-4 text-center border-l-2 border-b-2 border-[#334155]">{element.name}</td>
+                <td className="py-2 px-4 text-center flex justify-center border-x-2 border-b-2 border-[#334155]">
+                  <button onClick={()=>handleEditCompany(element.id, element.name)} className="relative bg-[#1b3341] text-[#e2e5e5] px-3 py-2 w-fit rounded mr-2 flex items-center gap-2 hover:scale-105 transition-transform
+                  after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:h-[2px] after:w-0 after:bg-[#2ec7bc] after:origin-center after:transition-all after:duration-300
+                  hover:after:left-0 hover:after:w-full">
+                    <EditIcon />Editar
                   </button>
-                  <button 
-                  onClick={()=>handleDeleteCompany(element.id)}
-                  className="bg-red-600 text-white px-3 py-1 rounded flex">
-                    <DeleteIcon />
-                    Eliminar
+                  <button onClick={()=>handleDeleteCompany(element.id)} className="relative bg-[#1b3341] text-[#f52926] px-3 py-2 rounded flex items-center gap-2 hover:scale-105 transition-transform
+                  after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:h-[2px] after:w-0 after:bg-[#f52926] after:origin-center after:transition-all after:duration-300
+                  hover:after:left-0 hover:after:w-full">
+                    <DeleteIcon />Eliminar
                   </button>
                 </td>
               </tr>
