436b24fd9e9413d6d19181795792d4e91cb142c0
Aaron-Ccente <aronccente@gmail.com>
2025-07-15 21:07:56 -0500

    Componente para agregar y editar compoñias productoras

--- src/Components/Dashboard/Productores/AddNewCompany.js
+++ src/Components/Dashboard/Productores/AddNewCompany.js
@@ -40,39 +40,40 @@ function AddNewCompany({ viewModal }) {
       <form
         action=""
         onSubmit={handleSubmit}
-        className="bg-black/80 shadow-xl shadow-white border-4 border-white py-10 px-8 rounded-lg w-fit text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        className="bg-[#0c161e] border-2 border-[#334155] py-10 px-8 rounded-lg w-fit text-[#e2e5e5] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       >
-        <p className="text-2xl font-bold pb-4 px-20 text-red-700">
-          Crear nueva compalia de produccion
-        </p>
+        <p className='text-2xl font-bold pb-4 px-20 text-[#2ec7bc]'>Crear nueva compañía de producción</p>
         <div className="mb-3 flex flex-col gap-4 ">
           <label htmlFor="name">
             <strong>Nombre</strong>
           </label>
           <input
             type="text"
-            placeholder="Ingrese su nombre"
+            placeholder="Ingrese el nombre de la compañia"
             onChange={handleInput}
             name="name"
             id="name"
-            className="p-2 border-2 border-gray-200 text-black  focus:border-red-900 focus:outline-none rounded"
+            className="p-2 border-2 border-[#334155] bg-[#0c161e]  focus:border-[#2ec7bc] focus:outline-none rounded"
           />
           {errors && <span className="text-red-600">{errors}</span>}
         </div>
-
         <div className="flex items-center justify-center gap-4 pt-6">
           <button
             type="button"
             onClick={viewModal}
-            className="px-4 py-2 border-2 border-white/40 bg-red-900 hover:bg-white hover:text-red-900 text-white rounded "
+            className="relative bg-[#f52926] text-[#e2e5e5] px-3 py-2 w-fit rounded mr-2 flex items-center gap-2 hover:scale-105 transition-transform
+                after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:h-[2px] after:w-0 after:bg-[#e2e5e5] after:origin-center after:transition-all after:duration-300
+                hover:after:left-0 hover:after:w-full"
           >
             <strong>Cancelar</strong>
           </button>
           <button
             type="submit"
-            className="px-4 py-2 border-2 border-red-900 hover:bg-white hover:text-red-900 text-white rounded"
+            className="relative bg-[#176b81] text-[#e2e5e5] px-3 py-2 w-fit rounded mr-2 flex items-center gap-2 hover:scale-105 transition-transform
+                after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:h-[2px] after:w-0 after:bg-[#e2e5e5] after:origin-center after:transition-all after:duration-300
+                hover:after:left-0 hover:after:w-full"
           >
-            <strong>Crear compañia</strong>
+            <strong>Crear compañía</strong>
           </button>
         </div>
       </form>
